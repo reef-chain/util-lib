ff4d616c8462a5cdd026a47d14a771bccb75b422
boo-0x <boo_0x@tutanota.com>
2023-05-30 10:09:13 +0200

    Update network config

--- src/network/network.ts
+++ src/network/network.ts
@@ -8,6 +8,9 @@ export interface Network {
   graphqlUrl: string;
   genesisHash: string;
   reefscanFrontendUrl: string;
+  factoryAddress: string;
+  routerAddress: string;
+  graphqlDexsUrl: string;
 }
 
 export const SS58_REEF = 42;
@@ -21,10 +24,12 @@ export const AVAILABLE_NETWORKS: Networks = {
     reefscanUrl: "https://testnet.reefscan.info",
     verificationApiUrl: "https://api-testnet.reefscan.com",
     graphqlUrl: "wss://squid.subsquid.io/reef-explorer-testnet/graphql",
-    // graphqlUrl: 'wss://testnet.reefscan.com/graphql',
     genesisHash:
       "0xb414a8602b2251fa538d38a9322391500bd0324bc7ac6048845d57c37dd83fe6",
     reefscanFrontendUrl: "https://testnet.reefscan.info",
+    factoryAddress: "0x06D7a7334B9329D0750FFd0a636D6C3dFA77E580",
+    routerAddress: "0xa29DFc7329ac30445Ba963E313fD26E171722057",
+    graphqlDexsUrl: "https://squid.subsquid.io/reef-swap-testnet/graphql",
   },
   mainnet: {
     name: "mainnet",
@@ -32,10 +37,12 @@ export const AVAILABLE_NETWORKS: Networks = {
     reefscanUrl: "https://reefscan.info",
     verificationApiUrl: "https://api.reefscan.com",
     graphqlUrl: "wss://squid.subsquid.io/reef-explorer/graphql",
-    // graphqlUrl: 'wss://reefscan.com/graphql',
     genesisHash:
       "0x7834781d38e4798d548e34ec947d19deea29df148a7bf32484b7b24dacf8d4b7",
     reefscanFrontendUrl: "https://reefscan.info",
+    factoryAddress: "0x380a9033500154872813F6E1120a81ed6c0760a8",
+    routerAddress: "0x641e34931C03751BFED14C4087bA395303bEd1A5",
+    graphqlDexsUrl: "https://squid.subsquid.io/reef-swap/graphql",
   },
   localhost: {
     name: "localhost",
@@ -43,7 +50,10 @@ export const AVAILABLE_NETWORKS: Networks = {
     reefscanUrl: "http://localhost:8000",
     verificationApiUrl: "http://localhost:8001",
     graphqlUrl: "ws://localhost:8080/v1/graphql",
-    genesisHash: "", // TODO ?
+    genesisHash: "",
     reefscanFrontendUrl: "http://localhost:3000",
+    factoryAddress: "",
+    routerAddress: "",
+    graphqlDexsUrl: "http://localhost:4351/graphql",
   },
 };
