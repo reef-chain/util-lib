8afd7bb09ea94bbe102dfc01119d8f7c3b8d3e8b
boo-0x <boo_0x@tutanota.com>
2023-05-31 15:03:44 +0200

    Update dex config

--- src/network/network.ts
+++ src/network/network.ts
@@ -8,9 +8,6 @@ export interface Network {
   graphqlUrl: string;
   genesisHash: string;
   reefscanFrontendUrl: string;
-  factoryAddress: string;
-  routerAddress: string;
-  graphqlDexsUrl: string;
 }
 
 export const SS58_REEF = 42;
@@ -27,9 +24,6 @@ export const AVAILABLE_NETWORKS: Networks = {
     genesisHash:
       "0xb414a8602b2251fa538d38a9322391500bd0324bc7ac6048845d57c37dd83fe6",
     reefscanFrontendUrl: "https://testnet.reefscan.info",
-    factoryAddress: "0x06D7a7334B9329D0750FFd0a636D6C3dFA77E580",
-    routerAddress: "0xa29DFc7329ac30445Ba963E313fD26E171722057",
-    graphqlDexsUrl: "https://squid.subsquid.io/reef-swap-testnet/graphql",
   },
   mainnet: {
     name: "mainnet",
@@ -40,9 +34,6 @@ export const AVAILABLE_NETWORKS: Networks = {
     genesisHash:
       "0x7834781d38e4798d548e34ec947d19deea29df148a7bf32484b7b24dacf8d4b7",
     reefscanFrontendUrl: "https://reefscan.info",
-    factoryAddress: "0x380a9033500154872813F6E1120a81ed6c0760a8",
-    routerAddress: "0x641e34931C03751BFED14C4087bA395303bEd1A5",
-    graphqlDexsUrl: "https://squid.subsquid.io/reef-swap/graphql",
   },
   localhost: {
     name: "localhost",
@@ -52,8 +43,5 @@ export const AVAILABLE_NETWORKS: Networks = {
     graphqlUrl: "ws://localhost:8080/v1/graphql",
     genesisHash: "",
     reefscanFrontendUrl: "http://localhost:3000",
-    factoryAddress: "",
-    routerAddress: "",
-    graphqlDexsUrl: "http://localhost:4351/graphql",
   },
 };
