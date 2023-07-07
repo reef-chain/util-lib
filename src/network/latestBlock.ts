e228a3260a792aca1d4edcc2c1ff9b37c3b08eb6
matjazonline <matjazonline@gmail.com>
2023-07-07 14:00:56 +0200

    get latest block observable

--- src/network/latestBlock.ts
+++ src/network/latestBlock.ts
@@ -1,10 +1,11 @@
 import Pusher from "pusher-js";
-import { from, Observable, shareReplay, switchMap, tap } from "rxjs";
+import { from, map, Observable, shareReplay, switchMap, tap } from "rxjs";
+import { filter } from "rxjs/operators";
 
 const PUSHER_KEY = "fc5ad78eb31981de6c67";
 const APP_CLUSTER = "eu";
 let pusherClient;
-let block$: Observable<LatestBlock>;
+let block$: Observable<PusherLatestBlock>;
 
 const getPusher = async () => {
   if (!pusherClient) {
@@ -31,27 +32,74 @@ interface LatestBlock {
   blockHash: string;
   blockHeight: number;
   blockId: string;
-  updatedEvmAddresses: string[];
-  updatedNativeAddresses: string[];
 }
 
-export const getLatestBlock$ = (): Observable<LatestBlock> => {
-  if (!block$) {
-    block$ = from(getPusher()).pipe(
-      switchMap((pusher: Pusher) => {
-        return new Observable<LatestBlock>(obs => {
-          const channel = pusher.subscribe("reef-chain");
-          channel.bind("block-finalised", data => {
-            obs.next(data);
-          });
+interface PusherLatestBlock extends LatestBlock {
+  updatedEvmAccounts: string[];
+  updatedNativeAccounts: string[];
+  updatedContracts: string[];
+}
 
-          return () => {
-            channel.unsubscribe();
-          };
-        });
-      }),
-      shareReplay(1)
-    );
-  }
-  return block$;
-};
+interface LatestAddressUpdates extends LatestBlock {
+  addresses: string[];
+}
+
+const latestBlockUpdates$ = from(getPusher()).pipe(
+  switchMap((pusher: Pusher) => {
+    return new Observable<PusherLatestBlock>(obs => {
+      const channel = pusher.subscribe("reef-chain");
+      channel.bind("block-finalised", data => {
+        obs.next(data);
+      });
+
+      return () => {
+        channel.unsubscribe();
+      };
+    });
+  }),
+  shareReplay(1)
+);
+
+export const _getBlockAccountTokenUpdates$ = (
+  filterAccountAddresses: string[],
+  latestBlockUpdates: Observable<PusherLatestBlock>
+): Observable<LatestAddressUpdates> =>
+  latestBlockUpdates.pipe(
+    map((blockUpdates: PusherLatestBlock) => {
+      const allUpdatedAccounts = blockUpdates.updatedEvmAccounts.concat(
+        blockUpdates.updatedNativeAccounts
+      );
+      if (
+        !filterAccountAddresses ||
+        !filterAccountAddresses.filter(v => !!v).length
+      ) {
+        return {
+          ...blockUpdates,
+          addresses: allUpdatedAccounts,
+        } as LatestAddressUpdates;
+      }
+      const filtered = allUpdatedAccounts.filter(addr =>
+        filterAccountAddresses.some(a => addr.trim() === a.trim())
+      );
+      return { ...blockUpdates, addresses: filtered } as LatestAddressUpdates;
+    }),
+    filter(v => v != null && !!v.addresses.length)
+  );
+
+export const blockAccountTokenUpdates$ = (filterAccountAddresses: string[]) =>
+  _getBlockAccountTokenUpdates$(filterAccountAddresses, latestBlockUpdates$);
+
+export const blockContractEvents$ = (
+  filterContractAddresses?: string[]
+): Observable<string[]> =>
+  latestBlockUpdates$.pipe(
+    map((blockUpdates: PusherLatestBlock): string[] => {
+      if (!filterContractAddresses || !filterContractAddresses.length) {
+        return blockUpdates.updatedContracts;
+      }
+      return blockUpdates.updatedContracts.filter(addr =>
+        filterContractAddresses.some(a => addr.trim() === a.trim())
+      );
+    }),
+    filter(v => v != null && !!v.length)
+  );
--- test/network/latestBlock.test.ts
+++ test/network/latestBlock.test.ts
@@ -1,19 +1,94 @@
+import { describe, expect, it } from "vitest";
 import {
-  initProvider,
-  disconnectProvider,
-} from "../../src/network/providerUtil";
-import { Provider } from "@reef-defi/evm-provider";
-import { describe, it, expect, beforeAll, afterAll } from "vitest";
-import { getLatestBlock$ } from "../../src/network/latestBlock";
-import { firstValueFrom } from "rxjs";
+  blockAccountTokenUpdates$,
+  _getBlockAccountTokenUpdates$,
+} from "../../src/network/latestBlock";
+import { firstValueFrom, Observable, Subject } from "rxjs";
 
 describe("Latest block functions", () => {
   it("should get latest block", async () => {
-    const latestBlock$ = getLatestBlock$();
+    const pusherLatestBlockSubj = new Subject();
+    setTimeout(function () {
+      pusherLatestBlockSubj.next({
+        blockHeight: 6699625,
+        blockId: "0006699625-64afe",
+        blockHash:
+          "0x64afe1c5db31ed4a6e2657f8c3afb102739a50b4dc04fdb3a775df16e82ec096",
+        updatedNativeAccounts: [
+          "5EnY9eFwEDcEJ62dJWrTXhTucJ4pzGym4WZ2xcDKiT3eJecP",
+          "5G9f52Dx7bPPYqekh1beQsuvJkhePctWcZvPDDuhWSpDrojN",
+        ],
+        updatedEvmAccounts: [
+          "0x7Ca7886e0b851e6458770BC1d85Feb6A5307b9a2",
+          "0xfb730ec3f38aB358AafA2EdD3fB2C17a5337dD7C",
+        ],
+        updatedContracts: ["0xDA04bA1313d382374fDB1834Ed0dF441b51636C8"],
+      });
+    }, 1000);
+    const latestBlock$ = _getBlockAccountTokenUpdates$(
+      [
+        "0x7Ca7886e0b851e6458770BC1d85Feb6A5307b9a2",
+        "0xfb730ec3f38aB358AafA2EdD3fB2C17a5337dD7C",
+      ],
+      pusherLatestBlockSubj.asObservable() as Observable<any>
+    );
     const block = await firstValueFrom(latestBlock$);
     expect(block).toBeDefined();
     expect(block.blockHash).toBeDefined();
     expect(block.blockId).toBeDefined();
     expect(block.blockHeight > 0).toBeTruthy();
+    expect(block.addresses.length).toBe(2);
+
+    const pusherLatestBlockSubj1 = new Subject();
+    setTimeout(function () {
+      pusherLatestBlockSubj1.next({
+        blockHeight: 6699625,
+        blockId: "0006699625-64afe",
+        blockHash:
+          "0x64afe1c5db31ed4a6e2657f8c3afb102739a50b4dc04fdb3a775df16e82ec096",
+        updatedNativeAccounts: [
+          "5EnY9eFwEDcEJ62dJWrTXhTucJ4pzGym4WZ2xcDKiT3eJecP",
+          "5G9f52Dx7bPPYqekh1beQsuvJkhePctWcZvPDDuhWSpDrojN",
+        ],
+        updatedEvmAccounts: [
+          "0x7Ca7886e0b851e6458770BC1d85Feb6A5307b9a2",
+          "0xfb730ec3f38aB358AafA2EdD3fB2C17a5337dD7C",
+        ],
+        updatedContracts: ["0xDA04bA1313d382374fDB1834Ed0dF441b51636C8"],
+      });
+    }, 3000);
+    const latestBlock1$ = _getBlockAccountTokenUpdates$(
+      ["0x7Ca7886e0b851e6458770BC1d85Feb6A5307b9a2"],
+      pusherLatestBlockSubj1.asObservable() as Observable<any>
+    );
+
+    const block1 = await firstValueFrom(latestBlock1$);
+    expect(block1.addresses.length).toBe(1);
+
+    const pusherLatestBlockSubj2 = new Subject();
+    setTimeout(function () {
+      pusherLatestBlockSubj2.next({
+        blockHeight: 6699625,
+        blockId: "0006699625-64afe",
+        blockHash:
+          "0x64afe1c5db31ed4a6e2657f8c3afb102739a50b4dc04fdb3a775df16e82ec096",
+        updatedNativeAccounts: [
+          "5EnY9eFwEDcEJ62dJWrTXhTucJ4pzGym4WZ2xcDKiT3eJecP",
+          "5G9f52Dx7bPPYqekh1beQsuvJkhePctWcZvPDDuhWSpDrojN",
+        ],
+        updatedEvmAccounts: [
+          "0x7Ca7886e0b851e6458770BC1d85Feb6A5307b9a2",
+          "0xfb730ec3f38aB358AafA2EdD3fB2C17a5337dD7C",
+        ],
+        updatedContracts: ["0xDA04bA1313d382374fDB1834Ed0dF441b51636C8"],
+      });
+    }, 1000);
+    const latestBlock2$ = _getBlockAccountTokenUpdates$(
+      [""],
+      pusherLatestBlockSubj2.asObservable() as Observable<any>
+    );
+
+    const block2 = await firstValueFrom(latestBlock2$);
+    expect(block2.addresses.length).toBe(4);
   });
 });
