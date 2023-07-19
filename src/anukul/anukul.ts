9706c31f21cd6aa6b5b05a45c31b3f8328ea5858
Anukul Pandey <anukul.030601@gmail.com>
2023-07-19 23:01:41 +0530

    feat: removed anukul tests

--- src/anukul/anukul.ts
+++
@@ -1,3 +0,0 @@
-export const helloAnukul = () => {
-  return "hello anukul";
-};
--- test/anukul/helloAnukul.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect } from "vitest";
-import { helloAnukul } from "../../src/anukul/anukul";
-
-describe("helloAnukul", () => {
-  it("should return hello anukul", () => {
-    const result = helloAnukul();
-    expect(result).toEqual("hello anukul");
-  });
-});
