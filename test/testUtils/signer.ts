8d5ed554eb6057c37b82a052ea85e69ed4b1037e
Anukul Pandey <anukul.030601@gmail.com>
2023-07-20 21:10:54 +0530

    feat: get signer

---
+++ test/testUtils/signer.ts
@@ -0,0 +1,13 @@
+import { Provider, Signer } from "@reef-defi/evm-provider";
+import { MnemonicSigner } from "./mnemonic-signer";
+
+export const getSigner = async (
+  provider: Provider,
+  address: string,
+  mnemonic?: string
+) => {
+  const hardCodedMnemonic =
+    "judge box bless much media say shrug crunch gun scorpion afraid object";
+  const implementedSigner = new MnemonicSigner(mnemonic ?? hardCodedMnemonic);
+  return new Signer(provider, address, implementedSigner as any);
+};
