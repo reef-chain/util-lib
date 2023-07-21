5876d5316b055669b5522da1dc7f2a16b24ebde5
Anukul Pandey <anukul.030601@gmail.com>
2023-07-21 15:48:19 +0530

    feat: prices test

--- test/token/prices.test.ts
+++ test/token/prices.test.ts
@@ -1,5 +1,9 @@
 import { describe, it, expect } from "vitest";
-import { getTokenPrice } from "../../src/token/prices";
+import {
+  getTokenPrice,
+  getTokenListPrices,
+  retrieveReefCoingeckoPrice,
+} from "../../src/token/prices";
 import { REEF_TOKEN, isNativeTransfer } from "../../src/token/index";
 
 describe("prices", () => {
@@ -7,8 +11,29 @@ describe("prices", () => {
     const res = await getTokenPrice("reef");
     expect(res).toBeGreaterThan(0.0);
   });
-  it("should return true", () => {
+  it("should return bool for native transfer", () => {
     const res = isNativeTransfer(REEF_TOKEN);
     expect(res).toEqual(true);
+  });
+});
+
+describe("getTokenListPrices", () => {
+  it("should return valid prices for a list of existing tokens", async () => {
+    const prices = await getTokenListPrices(["ethereum", "bitcoin"]);
+    expect(prices.ethereum).toBeGreaterThan(0);
+    expect(prices.bitcoin).toBeGreaterThan(0);
+  });
+
+  it("should not return a price for a non-existent token", async () => {
+    const prices = await getTokenListPrices(["ethereum", "nonexistenttoken"]);
+    expect(prices.ethereum).toBeGreaterThan(0);
+    expect(prices.nonexistenttoken).toBeUndefined();
+  });
+});
+
+describe("retrieveReefCoingeckoPrice", () => {
+  it("should return a valid price for the reef token", async () => {
+    const price = await retrieveReefCoingeckoPrice();
+    expect(price).toBeGreaterThan(0);
   });
 });
