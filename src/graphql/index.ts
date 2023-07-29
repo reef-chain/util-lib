3ee3871ae8a79ebbc179e8e6d687f9ede8ab350f
matjazonline <matjazonline@gmail.com>
2023-07-29 14:22:17 +0200

    export httpClientInstance$

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -8,3 +8,4 @@ export { TRANSFER_HISTORY_QUERY } from "./transferHistory.gql";
 export { EVM_ADDRESS_UPDATE_QUERY } from "./accounts.gql";
 export { getContractDataQuery } from "./contractData.gql";
 export { queryGql$ } from "./gqlUtil";
+export { httpClientInstance$ } from "./httpClient";
