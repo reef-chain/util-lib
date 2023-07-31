659fff02b0c1918be9ab0cce0e01382fea72ff54
Anukul Pandey <anukul.030601@gmail.com>
2023-07-31 17:02:09 +0530

    feat: build single dts op file

--- dts-bundle-generator.config.ts
+++ dts-bundle-generator.config.ts
@@ -1,18 +1,38 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const packageJson = require("./package.json");
+const fs = require("fs");
+const path = require("path");
 
 const getPackageName = () => {
   return packageJson.name.split("/")[1];
 };
 
+const findFilesWithIndexTs = dirPath => {
+  const entries = [];
+  fs.readdirSync(dirPath).forEach(file => {
+    const fullPath = path.join(dirPath, file);
+    if (fs.statSync(fullPath).isDirectory()) {
+      entries.push(...findFilesWithIndexTs(fullPath));
+    } else if (file.endsWith("index.ts")) {
+      entries.push({
+        filePath: `./${path.relative("./", fullPath)}`,
+        outFile: `./dist/${getPackageName()}.d.ts`,
+        noCheck: true,
+      });
+    }
+  });
+  return entries;
+};
+
+const srcDirectory = "./src";
+
+const entries = findFilesWithIndexTs(srcDirectory);
+
 const config = {
-  entries: [
-    {
-      filePath: "./src/index.ts",
-      outFile: `./dist/${getPackageName()}.d.ts`,
-      noCheck: true,
-    },
-  ],
+  compilationOptions: {
+    preferredConfigPath: "./tsconfig.json",
+  },
+  entries: [...entries],
 };
 
 module.exports = config;
