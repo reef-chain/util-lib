9e32077f731e60548c7ecb68625afbe6f5286a8b
Anukul Pandey <anukul.030601@gmail.com>
2023-12-12 13:09:55 +0530

    test: token icon url

--- test/token/tokenIcons.test.ts
+++ test/token/tokenIcons.test.ts
@@ -1,9 +1,7 @@
-import { beforeAll, describe, it } from "vitest";
+import { beforeAll, describe, expect, it } from "vitest";
 import { initReefState, selectedTokenBalances$ } from "../../src/reefState";
 import { AVAILABLE_NETWORKS } from "../../src/network";
 import { firstValueFrom, skip } from "rxjs";
-const axios = require("axios");
-const { from, map } = require("rxjs");
 
 describe("graphql tests", () => {
   const accountId = "5EnY9eFwEDcEJ62dJWrTXhTucJ4pzGym4WZ2xcDKiT3eJecP";
@@ -25,10 +23,12 @@ describe("graphql tests", () => {
 
   it("should log list of tokens", async () => {
     const response = await firstValueFrom(selectedTokenBalances$.pipe(skip(2)));
-    console.log(
-      response?.find(
-        token => token.address === "0x6685129d58bbf9FCd567770eFf1b9875e1fA24Bc"
-      )
-    );
+    response?.map(token => {
+      if (token.address == "0x6685129d58bbf9FCd567770eFf1b9875e1fA24Bc") {
+        expect(token.iconUrl).equal(
+          "https://cloudflare-ipfs.com/ipfs/QmQcGADrP24Jj76aZLnvFha2kPhMqhsdoCtF9tq7Lj65HZ"
+        );
+      }
+    });
   });
 });
