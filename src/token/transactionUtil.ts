fe81c9c3f7ca856bd9e80b4e7d4ba99f145363a4
matjazonline <matjazonline@gmail.com>
2024-01-03 13:05:25 +0100

    cleanup

--- src/token/transactionUtil.ts
+++ src/token/transactionUtil.ts
@@ -19,31 +19,6 @@ export interface TxStatusUpdate {
   addresses?: string[];
 }
 
-/*function toTxErrorCodeValue(e: { message: string } | string) {
-  let message = e.message || e;
-  let code = TX_STATUS_ERROR_CODE.ERROR_UNDEFINED;
-  if (
-      message
-      && (message.indexOf('-32603: execution revert: 0x') > -1
-          || message?.indexOf('InsufficientBalance') > -1)
-  ) {
-    message = 'You must allow minimum 60 REEF on account for Ethereum VM transaction even if transaction fees will be much lower.';
-    code = TX_STATUS_ERROR_CODE.ERROR_MIN_BALANCE_AFTER_TX;
-  }
-  if (message && message?.startsWith('1010')) {
-    message = 'Balance too low.';
-    code = TX_STATUS_ERROR_CODE.ERROR_BALANCE_TOO_LOW;
-  }
-  if (message && message?.startsWith('balances.InsufficientBalance')) {
-    message = 'Balance too low for transfer and fees.';
-    code = TX_STATUS_ERROR_CODE.ERROR_BALANCE_TOO_LOW;
-  }
-  if (code === TX_STATUS_ERROR_CODE.ERROR_UNDEFINED) {
-    message = `Transaction error: ${message}`;
-  }
-  return {message, code};
-}*/
-
 export const handleErr = (
   e: { message: string } | string,
   txIdent: string,
@@ -61,47 +36,6 @@ export const handleErr = (
     addresses: [signer.address],
   });
 };
-
-/*
-export const nativeTransfer = async (amount: string, destinationAddress: string, provider: Provider, signer: ReefSigner): Promise<void> => {
-  await provider.api.tx.balances
-    .transfer(destinationAddress, amount)
-    .signAndSend(signer.address, { signer: signer.signer.signingKey });
-};
-
-export const sendToNativeAddress = (
-  provider: Provider,
-  signer: ReefSigner,
-  toAmt: BigNumber,
-  to: string,
-  txHandler: TxStatusHandler,
-): string => {
-  const txIdent = Math.random().toString(10);
-  const transfer = provider.api.tx.balances.transfer(to, toAmt.toString());
-  signer.signer.getSubstrateAddress().then((substrateAddress) => {
-    transfer
-      .signAndSend(
-        substrateAddress,
-        { signer: signer.signer.signingKey },
-        (res) => {
-          const txHash = transfer.hash.toHex();
-          txHandler({
-            txIdent,
-            txHash,
-            isInBlock: res.isInBlock,
-            isComplete: res.isFinalized,
-            addresses: [signer.address, to],
-          });
-        },
-      )
-      .catch((e) => {
-        console.log('sendToNativeAddress err=', e);
-        handleErr(e, txIdent, '', txHandler, signer);
-      });
-  });
-
-  return txIdent;
-};*/
 
 export const getExtrinsicUrl = (
   id: string,
