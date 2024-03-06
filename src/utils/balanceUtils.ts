a2f7a8e4e9c1634927bd9eb135615a69d1b93825
Anukul Pandey <anukul.030601@gmail.com>
2024-03-06 20:29:44 +0530

    feat: format balance

--- src/utils/balanceUtils.ts
+++ src/utils/balanceUtils.ts
@@ -1,8 +1,9 @@
 export { toReefBalanceDisplay } from "./utils";
-import BigNumber from "bignumber.js";
+import { BigNumber } from "ethers";
+import * as bigNumber from "bignumber.js";
 
-export const formatHumanAmount = (value = ""): string => {
-  let amount = new BigNumber(value.replaceAll(",", ""));
+const formatHumanAmount = (value = ""): string => {
+  let amount = new bigNumber.BigNumber(value.replaceAll(",", ""));
   let output = "";
 
   if (amount.isNaN()) return amount.toString();
@@ -22,7 +23,7 @@ export const formatHumanAmount = (value = ""): string => {
         .dividedBy(decPlaces);
 
       if (amount.isEqualTo(1000) && i < abbrev.length - 1) {
-        amount = BigNumber(1);
+        amount = bigNumber.BigNumber(1);
         i += 1;
       }
 
@@ -41,25 +42,87 @@ const toCurrencyFormat = (value: number): string =>
     currencyDisplay: "symbol",
   }).format(value);
 
-export const formatDisplayBalance = (
+const formatHumanReadableBalance = (
   value: string,
-  decimals = 18,
-  price = 1
+  decimals: number,
+  price: number,
+  symbol: string
 ): string => {
   // pass price in params if you are providing balance in token
-  const balanceValue = new BigNumber(value)
-    .div(new BigNumber(10).pow(decimals))
+  const balanceValue = new bigNumber.BigNumber(value)
+    .div(new bigNumber.BigNumber(10).pow(decimals))
     .multipliedBy(price)
     .toNumber();
 
-  const balance = new BigNumber(balanceValue);
+  const balance = new bigNumber.BigNumber(balanceValue);
+
+  const isSymbol = symbol.length > 0;
 
   if (balance.isNaN()) return "0";
 
   if (balance.isGreaterThanOrEqualTo(1000000)) {
     const humanReadableBalance = formatHumanAmount(balance.toString());
-    return `$${humanReadableBalance}`;
+    return isSymbol
+      ? `${humanReadableBalance} ${symbol}`
+      : `$${humanReadableBalance}`;
   }
 
-  return toCurrencyFormat(balance.toNumber());
+  return isSymbol
+    ? `${toCurrencyFormat(balance.toNumber()).split("$")[1]} ${symbol}`
+    : toCurrencyFormat(balance.toNumber());
+};
+
+const _zeroPadding = (val: string, num: number): string => {
+  while (num - 1 > 0) {
+    val = "0" + val;
+    num--;
+  }
+  return val;
+};
+
+const _checkIfAllZeroes = (num: string): boolean => {
+  for (var i = 0; i < num.length; i++) {
+    if (num[i] != "0") return false;
+  }
+  return true;
+};
+
+const _formatDouble = (value: number): string => {
+  if (value < 1000) {
+    return value.toFixed(0).toString();
+  } else if (value < 1000000) {
+    return `${(value / 1000).toFixed(1)}k`;
+  } else if (value < 1000000000) {
+    return `${(value / 1000000).toFixed(3)}M`;
+  } else if (value < 1000000000000) {
+    return `${(value / 1000000000).toFixed(3)}B`;
+  } else {
+    return `${(value / 1000000000000).toFixed(3)}T`;
+  }
+};
+
+export const formatDisplayBalance = (
+  val: BigNumber | string,
+  isHumanReadable = false,
+  symbol = "",
+  fraction = 4,
+  decimals = 18,
+  price = 1
+): string => {
+  const threshold = BigNumber.from("1000000000000000000");
+  if (isHumanReadable && typeof val == "string") {
+    return formatHumanReadableBalance(val, decimals, price, symbol);
+  }
+  if ((val as BigNumber).lt(threshold)) {
+    var zeroPadding = _zeroPadding(
+      val.toString(),
+      18 - val.toString().length
+    ).substring(0, fraction);
+    if (_checkIfAllZeroes(zeroPadding)) {
+      zeroPadding = `${zeroPadding.substring(0, fraction - 1)}1`;
+    }
+    return `0.${zeroPadding}`;
+  }
+  const balance = (val as BigNumber).div(threshold);
+  return `${_formatDouble(balance.toNumber())}`;
 };
--- test/utils/balanceUtils.test.ts
+++ test/utils/balanceUtils.test.ts
@@ -1,30 +1,76 @@
+import { BigNumber } from "ethers";
 import { formatDisplayBalance } from "../../src/utils/balanceUtils";
 import { describe, it, expect } from "vitest";
 
 describe("formatDisplayBalance", () => {
   it("should format value smaller than 1", () => {
-    const result = formatDisplayBalance("100000");
+    const result = formatDisplayBalance(BigNumber.from("100000"));
+    console.log(result);
+    expect(result).toEqual("0.0001");
+  });
+  it("should format small value correctly", () => {
+    const result = formatDisplayBalance(BigNumber.from("1000000000000000000"));
+    console.log(result);
+    expect(result).toEqual("1");
+  });
+  it("should format 1000 value to k", () => {
+    const result = formatDisplayBalance(
+      BigNumber.from("1200000000000000000000")
+    );
+    console.log(result);
+    expect(result).toEqual("1.2k");
+  });
+  it("should format 1000000 value to M", () => {
+    const result = formatDisplayBalance(
+      BigNumber.from("1200000000000000000000000")
+    );
+    console.log(result);
+    expect(result).toEqual("1.200M");
+  });
+  it("should format 1000000000 value to B", () => {
+    const result = formatDisplayBalance(
+      BigNumber.from("1200000000000000000000000000")
+    );
+    console.log(result);
+    expect(result).toEqual("1.200B");
+  });
+  it("should format value smaller than 1 in $", () => {
+    const result = formatDisplayBalance("100000", true);
     console.log(result);
     expect(result).toEqual("$0.00");
   });
-  it("should format small value correctly", () => {
-    const result = formatDisplayBalance("1000000000000000000");
+  it("should format small value correctly in $", () => {
+    const result = formatDisplayBalance("1000000000000000000", true);
     console.log(result);
     expect(result).toEqual("$1.00");
   });
-  it("should format 1000 value to k", () => {
-    const result = formatDisplayBalance("1200000000000000000000");
+  it("should format 1000 value to $", () => {
+    const result = formatDisplayBalance("1200000000000000000000", true);
     console.log(result);
     expect(result).toEqual("$1,200.00");
   });
-  it("should format 1000000 value to M", () => {
-    const result = formatDisplayBalance("1200000000000000000000000");
+  it("should format 1000000 value to $ M", () => {
+    const result = formatDisplayBalance("1200000000000000000000000", true);
     console.log(result);
     expect(result).toEqual("$1.2 M");
   });
-  it("should format 1000000000 value to B", () => {
-    const result = formatDisplayBalance("1200000000000000000000000000");
+  it("should format 1000000000 value to $ B", () => {
+    const result = formatDisplayBalance("1200000000000000000000000000", true);
     console.log(result);
     expect(result).toEqual("$1.2 B");
+  });
+  it("should format return value in REEF", () => {
+    const result = formatDisplayBalance(
+      "1200000000000000000000000000",
+      true,
+      "REEF"
+    );
+    console.log(result);
+    expect(result).toEqual("1.2 B REEF");
+  });
+  it("should format return value in REEF", () => {
+    const result = formatDisplayBalance("120000000000000000000", true, "REEF");
+    console.log(result);
+    expect(result).toEqual("120.00 REEF");
   });
 });
