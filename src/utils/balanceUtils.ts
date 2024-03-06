58a789b16017d7ba9f085ebad7a5cc948b9b90d5
Anukul Pandey <anukul.030601@gmail.com>
2024-03-06 19:36:36 +0530

    feat: update formatDisplayBalance

--- src/utils/balanceUtils.ts
+++ src/utils/balanceUtils.ts
@@ -1,47 +1,65 @@
 export { toReefBalanceDisplay } from "./utils";
-import { BigNumber } from "ethers";
+import BigNumber from "bignumber.js";
 
-const _zeroPadding = (val: string, num: number): string => {
-  while (num - 1 > 0) {
-    val = "0" + val;
-    num--;
-  }
-  return val;
-};
+export const formatHumanAmount = (value = ""): string => {
+  let amount = new BigNumber(value.replaceAll(",", ""));
+  let output = "";
 
-const _checkIfAllZeroes = (num: string): boolean => {
-  for (var i = 0; i < num.length; i++) {
-    if (num[i] != "0") return false;
-  }
-  return true;
-};
+  if (amount.isNaN()) return amount.toString();
 
-const _formatDouble = (value: number): string => {
-  if (value < 1000) {
-    return value.toFixed(0).toString();
-  } else if (value < 1000000) {
-    return `${(value / 1000).toFixed(1)}k`;
-  } else if (value < 1000000000) {
-    return `${(value / 1000000).toFixed(3)}M`;
-  } else if (value < 1000000000000) {
-    return `${(value / 1000000000).toFixed(3)}B`;
-  } else {
-    return `${(value / 1000000000000).toFixed(3)}T`;
-  }
-};
+  const decPlaces = 100;
+  const abbrev = ["k", "M", "B"];
 
-export const formatDisplayBalance = (val: BigNumber, fraction = 4) => {
-  const threshold = BigNumber.from("1000000000000000000");
-  if (val.lt(threshold)) {
-    var zeroPadding = _zeroPadding(
-      val.toString(),
-      18 - val.toString().length
-    ).substring(0, fraction);
-    if (_checkIfAllZeroes(zeroPadding)) {
-      zeroPadding = `${zeroPadding.substring(0, fraction - 1)}1`;
+  for (let i = abbrev.length - 1; i >= 0; i -= 1) {
+    // eslint-disable-next-line
+    const size = Math.pow(10, (i + 1) * 3);
+
+    if (amount.isGreaterThanOrEqualTo(size)) {
+      amount = amount
+        .times(decPlaces)
+        .dividedBy(size)
+        .integerValue()
+        .dividedBy(decPlaces);
+
+      if (amount.isEqualTo(1000) && i < abbrev.length - 1) {
+        amount = BigNumber(1);
+        i += 1;
+      }
+
+      output = `${amount.toString()} ${abbrev[i]}`;
+      break;
     }
-    return `0.${zeroPadding}`;
   }
-  const balance = val.div(threshold);
-  return `${_formatDouble(balance.toNumber())}`;
+
+  return output;
+};
+
+const toCurrencyFormat = (value: number): string =>
+  Intl.NumberFormat("en", {
+    style: "currency",
+    currency: "USD",
+    currencyDisplay: "symbol",
+  }).format(value);
+
+export const formatDisplayBalance = (
+  value: string,
+  decimals = 18,
+  price = 1
+): string => {
+  // pass price in params if you are providing balance in token
+  const balanceValue = new BigNumber(value)
+    .div(new BigNumber(10).pow(decimals))
+    .multipliedBy(price)
+    .toNumber();
+
+  const balance = new BigNumber(balanceValue);
+
+  if (balance.isNaN()) return "0";
+
+  if (balance.isGreaterThanOrEqualTo(1000000)) {
+    const humanReadableBalance = formatHumanAmount(balance.toString());
+    return `$${humanReadableBalance}`;
+  }
+
+  return toCurrencyFormat(balance.toNumber());
 };
