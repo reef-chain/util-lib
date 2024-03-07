1aac09d2b3670643a32a3d48ed3c06dedf69c228
Anukul Pandey <62092256+anukulpandey@users.noreply.github.com>
2024-03-07 15:42:34 +0530

    feat: update formatDisplayBalance (#48)
    * feat: update formatDisplayBalance
    
    * feat: format balance
    
    * test: symbol formatting
    
    * fix: func signature
    
    * merge
    
    ---------
    
    Co-authored-by: matjazonline <matjazonline@gmail.com>

--- src/utils/balanceUtils.ts
+++ src/utils/balanceUtils.ts
@@ -1,5 +1,82 @@
 export { toReefBalanceDisplay } from "./utils";
 import { BigNumber } from "ethers";
+import * as bigNumber from "bignumber.js";
+
+const formatHumanAmount = (value = ""): string => {
+  let amount = new bigNumber.BigNumber(value.replaceAll(",", ""));
+  let output = "";
+
+  if (amount.isNaN()) return amount.toString();
+
+  const decPlaces = 100;
+  const abbrev = ["k", "M", "B"];
+
+  for (let i = abbrev.length - 1; i >= 0; i -= 1) {
+    // eslint-disable-next-line
+    const size = Math.pow(10, (i + 1) * 3);
+
+    if (amount.isGreaterThanOrEqualTo(size)) {
+      amount = amount
+        .times(decPlaces)
+        .dividedBy(size)
+        .integerValue()
+        .dividedBy(decPlaces);
+
+      if (amount.isEqualTo(1000) && i < abbrev.length - 1) {
+        amount = bigNumber.BigNumber(1);
+        i += 1;
+      }
+
+      output = `${amount.toString()} ${abbrev[i]}`;
+      break;
+    }
+  }
+
+  return output;
+};
+
+const toCurrencyFormat = (value: number): string =>
+  Intl.NumberFormat("en", {
+    style: "currency",
+    currency: "USD",
+    currencyDisplay: "symbol",
+  }).format(value);
+
+const defaultTokenDetails: TokenBalance = {
+  price: 1,
+  decimals: 18,
+  symbol: "",
+};
+
+const formatHumanReadableBalance = (
+  value: string,
+  tokenDetails: TokenBalance
+): string => {
+  const _tokenDetails = { ...defaultTokenDetails, ...tokenDetails };
+  // pass price in params if you are providing balance in token
+  const balanceValue = new bigNumber.BigNumber(value)
+    .div(new bigNumber.BigNumber(10).pow(_tokenDetails.decimals))
+    .multipliedBy(_tokenDetails.price)
+    .toNumber();
+
+  const balance = new bigNumber.BigNumber(balanceValue);
+  const isSymbol = _tokenDetails.symbol.length > 0;
+
+  if (balance.isNaN()) return "0";
+
+  if (balance.isGreaterThanOrEqualTo(1000000)) {
+    const humanReadableBalance = formatHumanAmount(balance.toString());
+    return isSymbol
+      ? `${humanReadableBalance} ${_tokenDetails.symbol}`
+      : `$${humanReadableBalance}`;
+  }
+
+  return isSymbol
+    ? `${toCurrencyFormat(balance.toNumber()).split("$")[1]} ${
+        _tokenDetails.symbol
+      }`
+    : toCurrencyFormat(balance.toNumber());
+};
 
 const _zeroPadding = (val: string, num: number): string => {
   while (num - 1 > 0) {
@@ -30,9 +107,22 @@ const _formatDouble = (value: number): string => {
   }
 };
 
-export const formatDisplayBalance = (val: BigNumber, fraction = 4) => {
+interface TokenBalance {
+  symbol?: string;
+  decimals?: number;
+  price?: number;
+}
+
+export const formatDisplayBalance = (
+  val: BigNumber | string,
+  fraction = 4,
+  tokenDetails?: TokenBalance
+): string => {
   const threshold = BigNumber.from("1000000000000000000");
-  if (val.lt(threshold)) {
+  if (tokenDetails && typeof val == "string") {
+    return formatHumanReadableBalance(val, tokenDetails);
+  }
+  if ((val as BigNumber).lt(threshold)) {
     var zeroPadding = _zeroPadding(
       val.toString(),
       18 - val.toString().length
@@ -42,6 +132,6 @@ export const formatDisplayBalance = (val: BigNumber, fraction = 4) => {
     }
     return `0.${zeroPadding}`;
   }
-  const balance = val.div(threshold);
+  const balance = (val as BigNumber).div(threshold);
   return `${_formatDouble(balance.toNumber())}`;
 };
