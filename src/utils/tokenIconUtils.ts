86d9112f211273ba324a6bbbe3b17a6df08af738
Anukul Pandey <anukul.030601@gmail.com>
2024-07-16 16:23:45 +0530

    chore: cleanup of ref

--- src/utils/tokenIconUtils.ts
+++ src/utils/tokenIconUtils.ts
@@ -6,39 +6,6 @@ import { httpClientInstance$, queryGql$ } from "src/graphql";
 import { selectedNetwork$ } from "src/reefState";
 import { getIpfsUrl } from "src/token/nftUtil";
 
-//reference to fetch explorer token icon
-/*
-const getExplorerTokenIcon = async (nwName: string, tokenAddresses: string[]) => {
-  const response = await fetch(getExplorerUrl(nwName), {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(getTokenIconQuery(tokenAddresses)),
-  });
-
-  if (!response.ok) {
-    throw new Error('Network response was not ok');
-  }
-
-  const { data } = await response.json();
-  const tokenIconMap = {};
-  data.verifiedContracts.forEach((v) => {
-    if (v.contractData && v.contractData['iconUrl']) {
-      const iconUrl = v.contractData['iconUrl'].startsWith("ipfs://")
-          ? "https://reef.infura-ipfs.io/ipfs/" + v.contractData['iconUrl'].split("ipfs://")[1]
-          : v.contractData['iconUrl'];
-          tokenIconMap[v.id] =iconUrl;
-  } else {
-    tokenIconMap[v.id] =v.contractData['iconUrl'];
-  }
-  })
-
-  return tokenIconMap;
-};
-
-*/
-
 //reference to fetch token icon from explorer contract data
 /*
 
