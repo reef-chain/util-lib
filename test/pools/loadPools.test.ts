733f37801c7aed182d026aa5c16fe08851e7a9b8
Anukul Pandey <anukul.030601@gmail.com>
2024-08-08 12:24:51 +0530

    feat: replaced loadpool with indexer calls

---
+++ test/pools/loadPools.test.ts
@@ -0,0 +1,28 @@
+import { beforeAll, describe, it, expect } from "vitest";
+import { firstValueFrom, skip, skipWhile, tap } from "rxjs";
+import { initReefState, selectedPools_status$ } from "../../src/reefState";
+import { AVAILABLE_NETWORKS } from "../../src/network";
+import { fetchPools$, loadPool } from "../../src/pools/pools";
+
+describe("pools", () => {
+  const signingKey = {};
+
+  beforeAll(async () => {
+    initReefState({
+      network: AVAILABLE_NETWORKS.testnet,
+      jsonAccounts: {
+        accounts: [
+          {
+            address: "5EnY9eFwEDcEJ62dJWrTXhTucJ4pzGym4WZ2xcDKiT3eJecP",
+            isSelected: true,
+          },
+        ],
+        injectedSigner: signingKey,
+      },
+    });
+  });
+  it("should load pool details", async () => {
+    const res = await firstValueFrom(selectedPools_status$.pipe(skip(1)));
+    expect(res.data.length).toBeGreaterThan(0);
+  });
+});
