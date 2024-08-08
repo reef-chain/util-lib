a3b892776bc4c98c2ad739abeebb1550823b0a48
Anukul Pandey <anukul.030601@gmail.com>
2024-08-08 11:02:44 +0530

    chore: cleanup

--- src/pools/pools.ts
+++ src/pools/pools.ts
@@ -86,25 +86,6 @@ export const loadPool = async (
   };
 };
 
-/*export const loadPools = async (
-  tokens: Token[],
-  signer: Signer,
-  factoryAddress: string,
-): Promise<Pool[]> => {
-  const tokenCombinations = uniqueCombinations(tokens);
-  const pools: Pool[] = [];
-  for (let index = 0; index < tokenCombinations.length; index += 1) {
-    try {
-      const [token1, token2] = tokenCombinations[index];
-      /!* eslint-disable no-await-in-loop *!/
-      const pool = await loadPool(token1, token2, signer, factoryAddress);
-      /!* eslint-disable no-await-in-loop *!/
-      pools.push(pool);
-    } catch (e) {}
-  }
-  return pools;
-};*/
-
 const cachePool$: Map<
   string,
   Observable<StatusDataObject<Pool | null>>
